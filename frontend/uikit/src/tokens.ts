0d69c103d4934d1c6820c2cc9d256dc84fc90b55
pigeon-240514 <169760332+pigeon-240514@users.noreply.github.com>
2025-07-28 17:17:54 +0800

    Migrate to new contracts

--- frontend/uikit/src/tokens.ts
+++ frontend/uikit/src/tokens.ts
@@ -62,7 +62,7 @@ export const WBTC: CollateralToken  = {
   icon: tokenWBTC,
   name: "WBTC",
   symbol: "WBTC" as const,
-  collateralRatio: 1.17,
+  collateralRatio: 1.176470588235294117,
   decimals: 8,
 };
 
@@ -70,7 +70,7 @@ export const LINK: CollateralToken  = {
   icon: tokenLINK,
   name: "LINK",
   symbol: "LINK" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -78,7 +78,7 @@ export const UNI: CollateralToken  = {
   icon: tokenUNI,
   name: "UNI",
   symbol: "UNI" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -86,7 +86,7 @@ export const AAVE: CollateralToken  = {
   icon: tokenAAVE,
   name: "AAVE",
   symbol: "AAVE" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -94,7 +94,7 @@ export const YFI: CollateralToken  = {
   icon: tokenYFI,
   name: "YFI",
   symbol: "YFI" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -102,7 +102,7 @@ export const LDO: CollateralToken  = {
   icon: tokenLDO,
   name: "LDO",
   symbol: "LDO" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -110,7 +110,7 @@ export const CRV: CollateralToken  = {
   icon: tokenCRV,
   name: "CRV",
   symbol: "CRV" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -118,7 +118,7 @@ export const FXS: CollateralToken  = {
   icon: tokenFXS,
   name: "FRAX",
   symbol: "FXS" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -126,7 +126,7 @@ export const SKY: CollateralToken  = {
   icon: tokenSKY,
   name: "SKY",
   symbol: "SKY" as const,
-  collateralRatio: 1.33,
+  collateralRatio: 1.333333333333333333,
   decimals: 18,
 };
 
@@ -134,7 +134,7 @@ export const LQTY: CollateralToken  = {
   icon: tokenLQTY,
   name: "LQTY",
   symbol: "LQTY" as const,
-  collateralRatio: 1.53,
+  collateralRatio: 1.538461538461538461,
   decimals: 18,
 };
 
